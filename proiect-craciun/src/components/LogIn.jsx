ed2f9ee6fcf8e9a8f5389c8115e2c0802561a346
t00nc00p <t00nc00p1234@gmail.com>
2024-12-17 23:19:46 +0200

    Update LogIn.jsx

--- proiect-craciun/src/components/LogIn.jsx
+++ proiect-craciun/src/components/LogIn.jsx
@@ -13,7 +13,7 @@ function LogIn() {
 
   async function water (email, password) {
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://proiect-tic-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

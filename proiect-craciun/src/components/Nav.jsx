d7e7ebe3b94ec899906406dd550fc3790ccab90f
t00nc00p <t00nc00p1234@gmail.com>
2024-12-18 00:07:28 +0200

    Update Nav.jsx

--- proiect-craciun/src/components/Nav.jsx
+++ proiect-craciun/src/components/Nav.jsx
@@ -17,7 +17,7 @@ function Nav() {
 
   async function validateLogin(email, password) {
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://proiect-tic-backend.onrender.com/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
